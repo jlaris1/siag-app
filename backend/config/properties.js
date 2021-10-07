9ba430490087e324c6a69ad2663bfc952569d2f9
Francisco Lopez <francisco.lopez@loanpro.io>
2021-10-07 16:38:10 -0700

    commit 1

--- backend/config/properties.js
+++ backend/config/properties.js
@@ -1,6 +1,6 @@
 const { modelNames } = require("mongoose");
 
 module.exports = {
-    PORT: process.env.PORT || 3000,
+    PORT: process.env.PORT || 80,
     DB: 'mongodb+srv://rancho_latuna:tuna_ranch20@latuna.z4uik.gcp.mongodb.net/ranchoLaTuna?retryWrites=true&w=majority'
 }
